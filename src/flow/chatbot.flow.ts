b2e90671f39cd642fd3d33a6178236ca92b99e4d
markmoraocg <139255069+markmoraocg@users.noreply.github.com>
2024-02-15 15:43:46 -0600

    updating

--- src/flow/chatbot.flow.ts
+++ src/flow/chatbot.flow.ts
@@ -5,11 +5,11 @@ import { generatePaymentLink } from 'src/services/paypal';
  * Un flujo conversacion que responder a las palabras claves "hola", "buenas", ...
  */
 export default BotWhatsapp.addKeyword(BotWhatsapp.EVENTS.ACTION)
-    .addAnswer('¿Como es tu email? lo necesito para generar link de pago',{capture:true}, 
+    .addAnswer('¿Podrías darme tu email? lo necesito para generar link de',{capture:true}, 
     async(ctx, {state, fallBack}) => {
         
         if(!ctx.body.includes('@')){
-            return fallBack('Eyy!bro esto no es un email valido! ponte serio')
+            return fallBack('Esto no es un email valido! Revisalo e intentalo de nuevo')
         }
         await state.update({email:ctx.body.toLowerCase()})
     })
